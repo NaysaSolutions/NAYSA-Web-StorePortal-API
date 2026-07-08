57e552f6071a13a20ddf1945afcb5a0debc41641
Jomel Mendoza <146928773+JomelMendoza@users.noreply.github.com>
2026-07-08 10:29:26 +0800

    merge

--- NAYSA-Web-StorePortal-API/config/database.php
+++ NAYSA-Web-StorePortal-API/config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'sqlsrv'),
 
     /*
     |--------------------------------------------------------------------------
@@ -102,11 +102,11 @@ return [
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'SQL5107.site4now.net'),
             'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'db_a9be64_naysacloudcomp1'),
+            'username' => env('DB_USERNAME', 'db_a9be64_naysacloudcomp1_admin'),
+            'password' => env('DB_PASSWORD', 'P@ssw0rd'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
